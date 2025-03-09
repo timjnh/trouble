f437e9fd43992359d45f47be30e2ffc1bd8db22f
Tim_Jones <tjones@aurorasolar.com>
2025-03-09 08:58:37 -0400

    Starting training implementation

--- trouble.py
+++ trouble.py
@@ -1,20 +1,13 @@
 import random
 import asyncio
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
 from motor.motor_asyncio import AsyncIOMotorClient
 from beanie import init_beanie
 from typing import List, Tuple
 
 from trouble import Game, Board, RandomActionSelector, DefaultDie, Color, Peg
 from trouble.models import GameDocument, TurnModel, BoardModel
-
-parser = ArgumentParser(description="Generate games of trouble")
-parser.add_argument("--persist", action="store_true", help="Persist the game to mongo")
-parser.add_argument("--mongo-uri", type=str, default="mongodb://localhost:27017/", help="MongoDB URI")
-parser.add_argument("--mongo-db", type=str, default="trouble", help="MongoDB database name")
-parser.add_argument("--num-games", "-n", type=int, default=1, help="Number of games to generate")
-parser.add_argument("--verbose", "-v", action="store_true", help="Verbose output")
-args = parser.parse_args()
+from trouble.training import Trainer
 
 def play_game() -> Tuple[Game, List[TurnModel]]:
     board = Board()
@@ -52,10 +45,13 @@ def play_game() -> Tuple[Game, List[TurnModel]]:
 
     return game, turns
 
-async def main():
+async def connect_to_mongo(mongo_uri: str, mongo_db):
+    mongo_client = AsyncIOMotorClient(mongo_uri)
+    await init_beanie(database=mongo_client[mongo_db], document_models=[GameDocument])
+
+async def generate(args: Namespace):
     if args.persist:
-        mongo_client = AsyncIOMotorClient(args.mongo_uri)
-        await init_beanie(database=mongo_client[args.mongo_db], document_models=[GameDocument])
+        await connect_to_mongo(args.mongo_uri, args.mongo_db)
 
     for i in range(args.num_games):
         i += 1
@@ -69,4 +65,28 @@ async def main():
             game_document = GameDocument(turns=turns, winner=str(game.winner))
             await game_document.insert()
 
-asyncio.run(main())
+async def train(args: Namespace):
+    await connect_to_mongo(args.mongo_uri, args.mongo_db)
+
+    trainer = Trainer()
+    await trainer.train()
+    print("Training complete!")
+
+parser = ArgumentParser(description="Play, generate and train machine-learning solutions on the game of Trouble")
+subparsers = parser.add_subparsers(required=True)
+
+generate_parser = subparsers.add_parser("generate", help="Generate games for use in training")
+generate_parser.add_argument("--persist", action="store_true", help="Persist the game to mongo")
+generate_parser.add_argument("--mongo-uri", type=str, default="mongodb://localhost:27017/", help="MongoDB URI")
+generate_parser.add_argument("--mongo-db", type=str, default="trouble", help="MongoDB database name")
+generate_parser.add_argument("--num-games", "-n", type=int, default=1, help="Number of games to generate")
+generate_parser.add_argument("--verbose", "-v", action="store_true", help="Verbose output")
+generate_parser.set_defaults(call=generate)
+
+train_parser = subparsers.add_parser("train", help="Train a machine-learning model on saved games")
+train_parser.add_argument("--mongo-uri", type=str, default="mongodb://localhost:27017/", help="MongoDB URI")
+train_parser.add_argument("--mongo-db", type=str, default="trouble", help="MongoDB database name")
+train_parser.set_defaults(call=train)
+
+args = parser.parse_args()
+asyncio.run(args.call(args))
